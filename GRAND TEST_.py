445d6050fbfb716c789194228f983149816cf746
hoisurendra <52737319+hoisurendra@users.noreply.github.com>
2019-07-13 16:05:48 +0530

    Add files via upload

---
+++ GRAND TEST_.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+num1 = '12321'
+num1 = num.casefold()
+rev_str = reversed(num1
+                  if list(num1) == list(rev_str):
+print("PALLINDROME")
+else:
+    print("NOT PALLINDROME")
+
